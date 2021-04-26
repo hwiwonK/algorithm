02287975e73caad7479f5db001af547108721f63
Hwiwon KIM <67336936+hwiwonK@users.noreply.github.com>
2021-04-27 08:43:09 +0900

    Create quickSort.cpp

---
+++ quickSort.cpp
@@ -0,0 +1,32 @@
+void quickSort(int start, int end, int N) {
+
+	int temp;
+	int pivot = start;
+	int i = pivot + 1; //왼쪽
+	int j = end; //오른쪽
+
+	if (start >= end) return;
+
+	while (i <= j) {
+		while (i <= end && lineA[sortIdx[i]] <= lineA[sortIdx[pivot]]) {
+			i++;
+		}
+		while (j > start && lineA[sortIdx[j]] >= lineA[sortIdx[pivot]]) {
+			j--;
+		}
+
+		if (i > j) {
+			temp = sortIdx[j];
+			sortIdx[j] = sortIdx[pivot];
+			sortIdx[pivot] = temp;
+		}
+		else {
+			temp = sortIdx[i];
+			sortIdx[i] = sortIdx[j];
+			sortIdx[j] = temp;
+		}
+	}
+
+	quickSort(start, j - 1, N);
+	quickSort(j + 1, end, N);
+}
