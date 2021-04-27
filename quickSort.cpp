c3919866dec18a0112e04443cce9d556bbc5fd9f
Hwiwon KIM <67336936+hwiwonK@users.noreply.github.com>
2021-04-27 15:06:53 +0900

    Update quickSort.cpp

--- quickSort.cpp
+++ quickSort.cpp
@@ -1,3 +1,6 @@
+//====================================================================
+//indexing 이용하기
+
 void quickSort(int start, int end, int N) {
 
 	int temp;
@@ -24,6 +27,41 @@ void quickSort(int start, int end, int N) {
 			temp = sortIdx[i];
 			sortIdx[i] = sortIdx[j];
 			sortIdx[j] = temp;
+		}
+	}
+
+	quickSort(start, j - 1, N);
+	quickSort(j + 1, end, N);
+}
+
+//========================================================================
+//indexing 이용하기
+void quickSort(int start, int end, int N) {
+
+	int temp;
+	int pivot = start;
+	int i = pivot + 1; //왼쪽
+	int j = end; //오른쪽
+
+	if (start >= end) return;
+
+	while (i <= j) {
+		while (i <= end && lader1Dist[i] <= lader2Dist[pivot]) {
+			i++;
+		}
+		while (j > start && lader1Dist[j] >= lader2Dist[pivot]) {
+			j--;
+		}
+
+		if (i > j) {
+			temp = j;
+			j = pivot;
+			pivot = temp;
+		}
+		else {
+			temp = i;
+			i = j;
+			j = temp;
 		}
 	}
 
